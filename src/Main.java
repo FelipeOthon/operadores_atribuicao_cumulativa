2bad07f4980443dd6630b275d26bd4526a48b89a
FelipeOthon <felipeosantiago@gmail.com>
2022-11-06 11:17:27 -0300

    enviar repo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite os minutos gastos");
+        int minutos = scanner.nextInt();
+        double conta = 50;
+        if (minutos>100){
+            conta += (minutos - 100) * 2;
+        }
+
+        System.out.printf("Valor a pagar: %.2f%n", conta);
+
+        scanner.close();
     }
 }
